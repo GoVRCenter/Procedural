b35f5dcb12e4c2b6130d76654f902a0b9db56270
Justin <Hamltoe@gmail.com>
2019-03-24 23:05:26 -0400

    Tweaks and tests with texture generetation.

--- Source/Procedural/BP_Functions.cpp
+++ Source/Procedural/BP_Functions.cpp
@@ -11,19 +11,22 @@ UTexture2D*  UBP_Functions::NewTexture2D(int32 inSizeX, int32 inSizeY, EPixelFor
 	UTexture2D* t = UTexture2D::CreateTransient(inSizeX, inSizeY, inFormat);
 	FTexture2DMipMap& Mip =  t->PlatformData->Mips[0];
 	void* Data = Mip.BulkData.Lock(LOCK_READ_WRITE);
+	int32 stride = (int32)(sizeof(uint8) * 4);
 
 	//Image generation done here pixel by pixel:
 	TArray<FColor> colors;
-	int64 pixelStream= t->PlatformData->SizeX * t->PlatformData->SizeY;
-	for (int64 i = 0; i < pixelStream; i++) {
-		if (i % 16) { colors.Add(FColor::Emerald); }
-		else colors.Add(FColor::White);
+	//int64 pixelStream = t->PlatformData->SizeX * t->PlatformData->SizeY;
+	for (int64 i = 0; i < t->PlatformData->SizeX; i++) {
+		for (int64 j = 0; j < t->PlatformData->SizeY; j++) {
+			if (i % 4 || j % 2){ colors.Add(FColor::Black); }
+			else { colors.Add(FColor::White); }
+		}
 	}
+
 
 	// End image generation
 
 	//Now copy the generated image to the newly created texture file.
-	int32 stride = (int32)(sizeof(uint8) * 4);
 	//UE_LOG(LogTemp, Warning, TEXT("%d"), t->PlatformData->SizeX * t->PlatformData->SizeY * stride);
 	FMemory::Memcpy(Data, colors.GetData(), t->PlatformData->SizeX * t->PlatformData->SizeY * stride);
 	Mip.BulkData.Unlock();
